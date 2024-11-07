9a6356310f3ff646d46ad57ec82032701081519d
Andy Lin <andy0010502@gmail.com>
2024-11-07 15:42:13 +0800

    把LoginPartial更改機制擴大到整個NavBar，刪除原LoginPartial。

--- src/components/Layout/Navbar.vue
+++ src/components/Layout/Navbar.vue
@@ -13,15 +13,25 @@
     >
         <div class="container">
             <!-- Logo -->
-            <RouterLink class="navbar-brand font-weight-bolder me-auto" to="/" v-bind="$attrs"
-                :class="isTransparent ? 'text-white' : 'text-dark'">
+            <RouterLink
+                class="navbar-brand font-weight-bolder me-auto"
+                to="/"
+                v-bind="$attrs"
+                :class="isTransparent ? 'text-white' : 'text-dark'"
+            >
                 <img src="@/assets/logo.svg" class="navbar-brand-img" alt="main_logo" />
                 <span class="ms-1">RecipeCube</span>
             </RouterLink>
 
             <!-- RWD的下拉選單按鈕 -->
-            <button class="navbar-toggler shadow-none ms-2" type="button" @click="toggleNavbar"
-                aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+            <button
+                class="navbar-toggler shadow-none ms-2"
+                type="button"
+                @click="toggleNavbar"
+                aria-controls="navigation"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
                 <span class="navbar-toggler-icon mt-2">
                     <span class="navbar-toggler-bar bar1"></span>
                     <span class="navbar-toggler-bar bar2"></span>
@@ -30,51 +40,138 @@
             </button>
 
             <div class="collapse navbar-collapse" :class="{ show: isNavbarShown }">
-                <ul class="navbar-nav ms-auto text-center">
-                    <li class="nav-item">
-                        <RouterLink class="nav-link me-2" to="/inventory">
-                            <i class="fa-solid fa-box-open opacity-6 me-1" aria-hidden="true"
-                                :class="isTransparent ? 'text-white' : 'text-dark'" @click="isNavbarShown = false"></i>
-                            庫存管理
-                        </RouterLink>
-                    </li>
-                    <li class="nav-item">
-                        <RouterLink class="nav-link me-2" to="/buyandcook">
-                            <i class="fa-solid fa-utensils opacity-6 me-1" aria-hidden="true"
-                                :class="isTransparent ? 'text-white' : 'text-dark'" @click="isNavbarShown = false"></i>
-                            隨買隨煮
-                        </RouterLink>
-                    </li>
-                    <li class="nav-item">
-                        <RouterLink class="nav-link me-2" to="/addingredient">
-                            <i class="fa-solid fa-carrot opacity-6 me-1" aria-hidden="true"
-                                :class="isTransparent ? 'text-white' : 'text-dark'" @click="isNavbarShown = false"></i>
-                            加入食材
-                        </RouterLink>
-                    </li>
-                    <li class="nav-item">
-                        <RouterLink class="nav-link me-2" to="/customrecipe">
-                            <i class="fa-solid fa-pencil opacity-6 me-1" aria-hidden="true"
-                                :class="isTransparent ? 'text-white' : 'text-dark'" @click="isNavbarShown = false"></i>
-                            自訂食譜
-                        </RouterLink>
-                    </li>
-                    <li class="nav-item">
-                        <RouterLink class="nav-link me-2" to="/recipe">
-                            <i class="fa-solid fa-book-bookmark opacity-6 me-1" aria-hidden="true"
-                                :class="isTransparent ? 'text-white' : 'text-dark'" @click="isNavbarShown = false"></i>
-                            食譜總覽
-                        </RouterLink>
-                    </li>
-                    <li class="nav-item">
-                        <RouterLink class="nav-link me-2" to="/store">
-                            <i class="fa-brands fa-shopify opacity-6 me-1" aria-hidden="true"
-                                :class="isTransparent ? 'text-white' : 'text-dark'" @click="isNavbarShown = false"></i>
-                            食材採購
-                        </RouterLink>
-                    </li>
-                </ul>
-                <LoginPartial></LoginPartial>
+                <template v-if="!authStore.token || !authStore.checkTokenExpiry">
+                    <ul class="navbar-nav ms-auto text-center">
+                        <li class="nav-item">
+                            <RouterLink class="nav-link me-2" to="/buyandcook">
+                                <i
+                                    class="fa-solid fa-utensils opacity-6 me-1"
+                                    aria-hidden="true"
+                                    :class="isTransparent ? 'text-white' : 'text-dark'"
+                                    @click="isNavbarShown = false"
+                                ></i>
+                                隨買隨煮
+                            </RouterLink>
+                        </li>
+                        <li class="nav-item">
+                            <RouterLink class="nav-link me-2" to="/recipe">
+                                <i
+                                    class="fa-solid fa-book-bookmark opacity-6 me-1"
+                                    aria-hidden="true"
+                                    :class="isTransparent ? 'text-white' : 'text-dark'"
+                                    @click="isNavbarShown = false"
+                                ></i>
+                                食譜總覽
+                            </RouterLink>
+                        </li>
+                        <li class="nav-item">
+                            <RouterLink class="nav-link me-2" to="/store">
+                                <i
+                                    class="fa-brands fa-shopify opacity-6 me-1"
+                                    aria-hidden="true"
+                                    :class="isTransparent ? 'text-white' : 'text-dark'"
+                                    @click="isNavbarShown = false"
+                                ></i>
+                                食材採購
+                            </RouterLink>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav ms-auto text-center">
+                        <li class="nav-item">
+                            <RouterLink class="nav-link me-2" :to="{ name: 'signup' }">
+                                <i class="fa fa-user opacity-6 me-1"></i> 註冊
+                            </RouterLink>
+                        </li>
+                        <li class="nav-item">
+                            <RouterLink class="nav-link me-2" :to="{ name: 'signin' }">
+                                <i class="fas fa-sign-out-alt opacity-6 me-1"></i> 登入
+                            </RouterLink>
+                        </li>
+                    </ul>
+                </template>
+                <template v-else>
+                    <ul class="navbar-nav ms-auto text-center">
+                        <li class="nav-item">
+                            <RouterLink class="nav-link me-2" to="/inventory">
+                                <i
+                                    class="fa-solid fa-box-open opacity-6 me-1"
+                                    aria-hidden="true"
+                                    :class="isTransparent ? 'text-white' : 'text-dark'"
+                                    @click="isNavbarShown = false"
+                                ></i>
+                                庫存管理
+                            </RouterLink>
+                        </li>
+                        <li class="nav-item">
+                            <RouterLink class="nav-link me-2" to="/buyandcook">
+                                <i
+                                    class="fa-solid fa-utensils opacity-6 me-1"
+                                    aria-hidden="true"
+                                    :class="isTransparent ? 'text-white' : 'text-dark'"
+                                    @click="isNavbarShown = false"
+                                ></i>
+                                隨買隨煮
+                            </RouterLink>
+                        </li>
+                        <li class="nav-item">
+                            <RouterLink class="nav-link me-2" to="/addingredient">
+                                <i
+                                    class="fa-solid fa-carrot opacity-6 me-1"
+                                    aria-hidden="true"
+                                    :class="isTransparent ? 'text-white' : 'text-dark'"
+                                    @click="isNavbarShown = false"
+                                ></i>
+                                加入食材
+                            </RouterLink>
+                        </li>
+                        <li class="nav-item">
+                            <RouterLink class="nav-link me-2" to="/customrecipe">
+                                <i
+                                    class="fa-solid fa-pencil opacity-6 me-1"
+                                    aria-hidden="true"
+                                    :class="isTransparent ? 'text-white' : 'text-dark'"
+                                    @click="isNavbarShown = false"
+                                ></i>
+                                自訂食譜
+                            </RouterLink>
+                        </li>
+                        <li class="nav-item">
+                            <RouterLink class="nav-link me-2" to="/recipe">
+                                <i
+                                    class="fa-solid fa-book-bookmark opacity-6 me-1"
+                                    aria-hidden="true"
+                                    :class="isTransparent ? 'text-white' : 'text-dark'"
+                                    @click="isNavbarShown = false"
+                                ></i>
+                                食譜總覽
+                            </RouterLink>
+                        </li>
+                        <li class="nav-item">
+                            <RouterLink class="nav-link me-2" to="/store">
+                                <i
+                                    class="fa-brands fa-shopify opacity-6 me-1"
+                                    aria-hidden="true"
+                                    :class="isTransparent ? 'text-white' : 'text-dark'"
+                                    @click="isNavbarShown = false"
+                                ></i>
+                                食材採購
+                            </RouterLink>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav ms-auto text-center">
+                        <li class="nav-item">
+                            <RouterLink class="nav-link me-2" to="/user">
+                                <i class="fa fa-user opacity-6 me-1"></i>
+                                {{ authStore.userData.UserName }}
+                            </RouterLink>
+                        </li>
+                        <li @click="authStore.logout()" class="nav-item">
+                            <RouterLink class="nav-link me-2" to="/">
+                                <i class="fas fa-sign-out-alt opacity-6 me-1"></i> 登出
+                            </RouterLink>
+                        </li>
+                    </ul>
+                </template>
             </div>
         </div>
     </nav>
@@ -83,7 +180,10 @@
 
 <script setup>
 import { ref } from 'vue';
-import LoginPartial from './LoginPartial.vue';
+import { useAuthStore } from '@/stores/auth';
+import { useOAuthStore } from '@/stores/oauth';
+const authStore = useAuthStore();
+const oauthStore = useOAuthStore();
 
 const props = defineProps({
     isTransparent: {
@@ -99,5 +199,4 @@ const isNavbarShown = ref(false);
 const toggleNavbar = () => {
     isNavbarShown.value = !isNavbarShown.value;
 };
-
 </script>
