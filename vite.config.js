747e234a5645a6e6017f23fb402aa18aa06ef13a
Willy <Willy@ispan.com.tw>
2024-11-08 14:07:08 +0800

    1) 加入假https文件
    後續實作功能:
       -
    待優化功能:
    
    登入&註冊使用 VeeValidate來實現多語系資料驗證
    驗證email版面美化
    前端重設密碼token過期無法捕捉後端api message
    將新增偏好&不可食用食材改成ajax
    勾選是否紀錄偏好&不可食用食材
    群組改名功能
    加入群組後ajax刷新
    前端表單輸入驗證

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import fs from 'fs';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
     resolve: {
@@ -15,5 +15,14 @@ export default defineConfig({
         preprocessorOptions: {
             scss: { api: 'modern-compiler' },
         },
+    },
+    base: '/',  // 這裡保持為根路徑，或者你也可以設定為 /linetest/ 如果需要
+    server: {
+        https: {
+            key: fs.readFileSync(path.resolve(__dirname, 'certs', 'key.pem')),  // 私鑰檔案路徑
+            cert: fs.readFileSync(path.resolve(__dirname, 'certs', 'cert.pem')),  // 憑證檔案路徑
+        },
+        host: 'localhost',
+        port: 5173,
     },
 });
